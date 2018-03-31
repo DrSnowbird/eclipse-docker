ed84334f7f11ad98e18113361340f27eb2b2cfe1
Dr.Snowbird <DrSnowbird@openkbs.org>
2018-03-31 01:18:47 -0400

    update docker-compose, run.sh, build.sh

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,17 @@ FROM openkbs/jdk-mvn-py3-x11
 
 MAINTAINER DrSnowbird "DrSnowbird@openkbs.org"
 
-## ---- USER_NAME is defined in parent image: openkbs/jre-mvn-py3-x11 already ----
+## ---- USER_NAME is defined in parent image: openkbs/jdk-mvn-py3-x11 already ----
 ENV USER_NAME=${USER_NAME:-developer}
 ENV HOME=/home/${USER_NAME}
 ENV ECLIPSE_WORKSPACE=${HOME}/eclipse-workspace
-        
+
+ARG ELCIPSE_TAR=${ELCIPSE_TAR:-eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz}
+
 WORKDIR /opt
-RUN wget -c http://mirror.math.princeton.edu/pub/eclipse//technology/epp/downloads/release/oxygen/R/eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz && \
-    tar xvf eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz && \
-    rm eclipse-jee-oxygen-R-linux-gtk-x86_64.tar.gz 
+RUN wget -c http://mirror.math.princeton.edu/pub/eclipse//technology/epp/downloads/release/oxygen/R/${ELCIPSE_TAR} && \
+    tar xvf ${ELCIPSE_TAR} && \
+    rm ${ELCIPSE_TAR} 
     
 VOLUME ${ECLIPSE_WORKSPACE}
 VOLUME ${HOME}/.eclipse 
--- build.sh
+++ build.sh
@@ -1,24 +1,66 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 # Reference: 
 # - https://docs.docker.com/engine/userguide/containers/dockerimages/
 # - https://github.com/dockerfile/java/blob/master/oracle-java8/Dockerfile
 
-imageTag=openkbs/eclipse-oxygen-docker
+if [ $# -lt 1 ]; then
+    echo "Usage: "
+    echo "  ${0} <image_tag>"
+fi
+MY_DIR=$(dirname "$(readlink -f "$0")")
 
-docker build -t ${imageTag} \
+
+###################################################
+#### ---- Change this only if want to use your own
+###################################################
+ORGANIZATION=openkbs
+
+###################################################
+#### ---- Generate build-arg arguments ----
+###################################################
+BUILD_ARGS=""
+ARGS_DEFINITION_FILE="./docker.env"
+## -- ignore entries start with "#" symbol --
+function generateBuildArgs() {
+    for r in `cat ${ARGS_DEFINITION_FILE} | grep -v '^#'`; do
+        echo "entry=$r"
+        key=`echo $r | tr -d ' ' | cut -d'=' -f1`
+        value=`echo $r | tr -d ' ' | cut -d'=' -f2`
+        BUILD_ARGS="${BUILD_ARGS} --build-arg $key=$value"
+    done
+}
+generateBuildArgs
+echo "BUILD_ARGS=${BUILD_ARGS}"
+
+###################################################
+#### ---- Container package information ----
+###################################################
+DOCKER_IMAGE_REPO=`echo $(basename $PWD)|tr '[:upper:]' '[:lower:]'|tr "/: " "_" `
+imageTag=${1:-"${ORGANIZATION}/${DOCKER_IMAGE_REPO}"}
+
+docker build --rm -t ${imageTag} \
+    ${BUILD_ARGS} \
 	-f Dockerfile .
 
-echo "----> To run in interactive mode: "
+echo "----> Shell into the Container in interactive mode: "
+echo "  docker exec -it --name <some-name> /bin/bash"
+echo "e.g."
+echo "  docker run --name "my-$(basename $imageTag)" /bin/bash "
+
+echo "----> Run: "
 echo "  docker run --name <some-name> -it ${imageTag} /bin/bash"
 echo "e.g."
-echo "  docker run it ${imageTag} "
-echo "  docker run --name "my-$(basename $imageTag)" -it ${imageTag} "
+echo "  docker run --name "my-$(basename $imageTag)" ${imageTag} "
 
-echo "----> Docker Images"
+echo "----> Run in interactive mode: "
+echo "  docker run -it --name <some-name> ${imageTag} /bin/bash"
+echo "e.g."
+echo "  docker run -it --name "my-$(basename $imageTag)" -it ${imageTag} "
+
+echo "----> Build Docker Images again: "
 echo "To build again: (there is a dot at the end of the command!)"
 echo "  docker build -t ${imageTag} . "
 echo
 docker images |grep "$imageTag"
-
 
